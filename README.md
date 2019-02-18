9731a3864aa00dcd553ace5de2456f5a8ce07cc0
Toyokazu Tsugehara <tsugehara@gmail.com>
2019-02-18 16:17:13 +0900

    穴ぼこ空いてしまったがとりあえず動かした

---
+++ README.md
@@ -0,0 +1,22 @@
+# blog-server
+
+## このリポジトリについて
+
+[blog](https://cli.vuejs.org/)のFirebase側です。
+
+詳しくはそちらのリポジトリをご参照ください。
+
+## LICENSE
+
+特に意味はありませんがMITです。
+
+詳しくは[LICENSE](./LICENSE)をご参照ください。
+
+## 必要そうな情報
+
+- `firestore.rules` は使ってます
+    - `npm run deploy:firestore` でデプロイできます
+- `functions` は使ってます
+    - functionsにcdした後で`npm run deploy` でデプロイできます
+
+あとはまだ勉強中なのでテンプレのままです。
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,96 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+interface UserPost {
+	subject: string;
+	body: string;
+	created: FirebaseFirestore.Timestamp;
+	updated: FirebaseFirestore.Timestamp;
+}
+
+interface AllPost {
+	userId: string;
+	userName: string;
+	postRef: FirebaseFirestore.DocumentReference;
+	userDisplayName: string;
+	subject: string;
+	body: string;
+	created: FirebaseFirestore.Timestamp;
+	updated: FirebaseFirestore.Timestamp;
+}
+
+interface UserData {
+	id: string;
+	created: FirebaseFirestore.Timestamp;
+	name: string;
+	displayName: string;
+}
+
+async function copyToAllPost(snapshot: FirebaseFirestore.DocumentSnapshot, context: functions.EventContext) {
+	const userName: string = context.params.userName;
+	// const postId: string = context.params.postId;
+	const post = snapshot.data() as UserPost;
+	try {
+		const user = await firestore.collection("users").doc(userName).get();
+		const userData = user.data() as UserData;
+		const allPost = {
+			userId: userData.id,
+			userName: userData.name,
+			userDisplayName: userData.displayName,
+			postRef: snapshot.ref,
+			created: post.created,
+			updated: post.updated,
+			subject: post.subject,
+			body: post.body,
+		} as AllPost;
+		return await firestore.collection("allPosts").add(allPost);
+	} catch (error) {
+		return Promise.reject(error);
+	}
+}
+
+async function updateToAllPost(change: functions.Change<FirebaseFirestore.DocumentSnapshot>, context: functions.EventContext) {
+	const userName: string = context.params.userName;
+	// const postId: string = context.params.postId;
+	const oldPostRef = change.before!.ref
+	const post = change.after!.data() as UserPost;
+	try {
+		const user = await firestore.collection("users").doc(userName).get();
+		const userData = user.data() as UserData;
+		const allPost = {
+			userId: userData.id,
+			userName: userData.name,
+			userDisplayName: userData.displayName,
+			postRef: change.after!.ref,
+			created: post.created,
+			updated: post.updated,
+			subject: post.subject,
+			body: post.body,
+		} as AllPost;
+		const oldAllPosts = await firestore.collection("allPosts").where("postRef", "==", oldPostRef).get();
+		let count = 0;
+		const promises: Promise<FirebaseFirestore.WriteResult>[] = [];
+		oldAllPosts.forEach((oldAllPost) => {
+			if (++count > 1) {
+				// error!
+			}
+			promises.push(firestore.collection("allPosts").doc(oldAllPost.id).set(
+				allPost,
+				{merge: true}
+			));
+		});
+		if (count === 0) {
+			// error!
+		}
+		const result = await Promise.all(promises);
+		return result[0];
+	} catch (error) {
+		return Promise.reject(error);
+	}
+}
+
+
+export const onUserPostCreate = functions.firestore.document("/users/{userName}/posts/{postId}").onCreate(copyToAllPost);
+functions.firestore.document("/users/{userName}/posts/{postId}").onUpdate(updateToAllPost);
